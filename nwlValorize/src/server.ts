179f29a534e9b4e22c6027484fb6ace59b1fe6cc
Francisco Correia <franciscocorreia383@gmail.com>
2021-07-28 15:13:47 -0300

    Commit da primeira aula

---
+++ nwlValorize/src/server.ts
@@ -0,0 +1,14 @@
+import express, { response } from "express" ;
+
+const app = express();
+
+app.get("/test", (request, response) => {
+
+   return response.send("Olá NLW")
+})
+
+app.post("/test-post", (request, response) => {
+    return response.send("Olá NLW método Post")
+})
+
+app.listen(3000, () => console.log("Server is running"));
